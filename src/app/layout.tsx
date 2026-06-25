e5c5f3b19dcbf75a869ae3e87436958d7eb7cce9
Ahmed Ali <Ahmed97028@gmail.com>
2026-06-25 17:38:06 +0400

    Update Meta Pixel ID for marketing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '508677432337059');
+              fbq('init', '987163357456481');
               fbq('track', 'PageView');
             `,
           }}
@@ -68,7 +68,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=508677432337059&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=987163357456481&ev=PageView&noscript=1"
             alt="facebook pixel"
           />
         </noscript>
