6c447e38bcf94ccee67214c1fa2b5cffe4b7afb2
Ahmed Ali <Ahmed97028@gmail.com>
2026-07-13 12:29:52 +0400

    chore: add markdown dependencies and configure image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,22 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true, // Enable React's Strict Mode for better development practices
   images: {
-    domains: [
-      'images.unsplash.com',
-      'img.icons8.com',
-      'unsplash.com',
-    ],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: '*.public.blob.vercel-storage.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.icons8.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'unsplash.com',
       },
     ],
   },
